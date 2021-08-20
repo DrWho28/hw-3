4a119bd2e310cc0b7242346d838b9af26d093635
Andrei way <andreicho@Andreis-MacBook-Pro.local>
2021-08-19 21:31:58 -0400

    did a lot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hw-3
 added html and java and css 
+added a math.floor and random 
+added an element id 
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+/*// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 
@@ -14,11 +14,78 @@ function writePassword() {
   var password = generatePassword(); // 'Password'
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
 }
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);*/
+
+// DOM elements 
+const resultEL =document.getElementById("results")
+const lengthEL =document.getElementById("lengthEL")
+const uppercaseEL =document.getElementById("uppercase")
+const lowercaseEL =document.getElementById("lowercase")
+const numberEL =document.getElementById("numbers")
+const symbolEL =document.getElementById("symbol")
+const generateEL =document.getElementById("generate")
+const clipboardEL =document.getElementById("clipboard")
+const modal =document.getElementById("options")
+
+ const RandomFunc ={
+   number:getRadomNumber,
+   lower:getRandomLower,
+   symbol:getRandomSymbol,
+   upper:getRandomUpper,
+ }
+
+ /*generateEL.addEventListener ("click", (e) => {
+  //e.preventDefault()
+  //console.log(modal)
+  //modal.style.display="flex"
+}) */
+
+// generator function - https://www.w3schools.com/html/html_charset.asp
+
+function getRandomLower () {
+return string.fromCharCode(Math.floor (Math.random()* 26) + 97);
+}
+function getRandomUpper (){
+  return string.fromCharCode(Math.floor (Math.random()* 26) + 65)
+}
+function getRandomSymbol(){
+  const symbol ='!,@,#,$,%,^,&,*,(,),_,+,{,},|,",:,<,>,?,,/,.,;,],[,],';
+  return Symbol[Math.floor (Math.random() * Symbol.length )];
+}
+
+function getRadomNumber (){
+  return string.fromCharCode(Math.floor (Math.random()* 10) + 48)
+}
+ 
+modal.addEventListener("submit", (e) => {
+e.preventDefault()
+const uppercase = uppercaseEL.value === "y"
+const lowercase= lowercaseEL.value === "y"
+const number= numberEL.value (=== "y")
+const symbol=symbolEl.value (=== "y")
+function generatePassword (upper,lower,num,sym){
+let password=""
+if (upper === true){
+password += getRandomUpper()}
+if (lower === true){
+  password += getRandomLower()}
+if (num === true){
+  password += getRadomNumber()}
+  if (sym === true){
+    password += getRandomSymbol()}
+
+document.getElementById("password")
+.innerHTML=password
+return password
+  }
+modal.style.display=""
+console.log(password)
+})
+
